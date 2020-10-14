78d0beea21eff11bfa1ec7a7421178354f5d9b8b
Anfecare <72477054+Anfecare@users.noreply.github.com>
2020-10-14 10:07:22 -0500

    Commit de las 10

---
+++ app/Models/bodegas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class bodegas extends Model
+{
+    use HasFactory;
+    protected $table = 'bodegas';
+    protected $primarykey = 'id';
+}
---
+++ app/Models/detalle_facturas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class detalle_facturas extends Model
+{
+    use HasFactory;
+    protected $table = 'detalle_facturas';
+    protected $primarykey = 'id';
+}
